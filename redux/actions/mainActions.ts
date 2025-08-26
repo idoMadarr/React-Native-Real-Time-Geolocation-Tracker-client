b7458b4d837a7bae9a9f81aa8e98b8b9a52617fa
Ido madaRR <idox2x@gmail.com>
2025-08-26 23:30:14 +0300

    starting with off mode

--- redux/actions/mainActions.ts
+++ redux/actions/mainActions.ts
@@ -5,12 +5,29 @@ import {
   setRecords,
   updateRecordList,
 } from '../slices/mainSlice';
+import Config from 'react-native-config';
+import {analyzeRoadRecord} from '../../utils/haversineFormula';
 
 export const saveRecord = (body: any) => async (dispatch: Dispatch) => {
-  const data: any = await axiosInstance.post('/summarize', body);
-  if ('error' in data) return data;
+  if (Config.OFFLINE_MODE === 'true') {
+    const {distance, averageSpeed, startTime, endTime, waypoints} =
+      analyzeRoadRecord(body);
+    const currentRecord = {
+      distance,
+      averageSpeed,
+      startTime,
+      endTime,
+      waypoints,
+    };
+    console.log(currentRecord, 'result');
+    // ...saveToStorage
+    dispatch(setCurrentRecord(currentRecord));
+  } else {
+    const data: any = await axiosInstance.post('/summarize', body);
+    if ('error' in data) return data;
 
-  dispatch(setCurrentRecord(data));
+    dispatch(setCurrentRecord(data));
+  }
 };
 
 export const updateScreenShot =
@@ -24,11 +41,16 @@ export const updateScreenShot =
 
 export const fetchRecords =
   (deviceId: string) => async (dispatch: Dispatch) => {
-    const data = await axiosInstance.post('/device-records', {
-      deviceId,
-    });
-    // @ts-ignore:
-    if (data === false) return false;
+    if (Config.OFFLINE_MODE === 'true') {
+      // Fetch from local storage
+      // dispatch(setRecords(data));
+    } else {
+      const data = await axiosInstance.post('/device-records', {
+        deviceId,
+      });
+      // @ts-ignore:
+      if (data === false) return false;
 
-    dispatch(setRecords(data));
+      dispatch(setRecords(data));
+    }
   };

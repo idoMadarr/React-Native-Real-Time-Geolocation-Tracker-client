ee8a7f8440647d9bc0f21c2011d80ac158b1441c
Ido madaRR <idox2x@gmail.com>
2025-08-31 07:34:11 +0300

    keep awake

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import {Provider} from 'react-redux';
+import KeepAwake from 'react-native-keep-awake';
 import AppNavigation from './navigation/AppNavigation';
 import store from './redux/store/store';
-
 EStyleSheet.build({});
 
 const App = () => {
+  KeepAwake.activate();
+
   return (
     <Provider store={store}>
       <AppNavigation />
